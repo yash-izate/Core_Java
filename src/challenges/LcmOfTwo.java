a42f505f01dac89a38ae42d880f5d20e33549363
Yash Izate <yashizate.softech@gmail.com>
2026-01-11 19:21:28 +0530

    method calling

--- src/challenges/LcmOfTwo.java
+++ src/challenges/LcmOfTwo.java
@@ -29,18 +29,6 @@ public class LcmOfTwo {
         int num1 = userInput();
         int num2 = userInput();
 
-        System.out.println("\nWelcome to LCM Calculator of Two Numbers !..\n");
-        System.out.print("Enter first number: ");
-        int num1 = sc.nextInt();
-        System.out.print("Enter second number: ");
-        int num2 = sc.nextInt();
-        System.out.println();
-
-        for (int i = Math.max(num1, num2); i < num1 * num2; i += Math.max(num1, num2)) {
-            if ((i % num1 == 0) && (i % num2 == 0)) {
-                System.out.println("LCM (" + num1 + "," + num2 + ") = " + i);
-                break;
-            }
-        }
+        LCM(num1, num2);
     }
 }
