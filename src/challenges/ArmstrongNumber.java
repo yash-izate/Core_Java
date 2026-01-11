df0e3e5b8e9f8b6fb6f1ac98839b00660a571cfc
Yash Izate <yashizate.softech@gmail.com>
2026-01-12 04:41:21 +0530

    challenges

--- src/challenges/ArmstrongNumber.java
+++ src/challenges/ArmstrongNumber.java
@@ -4,6 +4,33 @@ import java.util.Scanner;
 
 public class ArmstrongNumber {
 
+    // Function to count digits
+    static int countDigits(int num) {
+        if (num == 0) {
+            return 1;
+        }
+
+        int digits = 0;
+        while (num > 0) {
+            digits++;
+            num /= 10;
+        }
+        return digits;
+    }
+
+    // Function to calculate Armstrong sum
+    static int calculateArmstrongSum(int num, int digits) {
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += (int) Math.pow(digit, digits);
+            num /= 10;
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -17,40 +44,8 @@ public class ArmstrongNumber {
             return;
         }
 
-        int sum = getSum(num);
-
-        if (sum == num) {
-            System.out.println("Armstrong Number");
-        } else {
-            System.out.println("Not an Armstrong Number");
-        }
-
-        sc.close();
-    }
-
-    private static int getSum(int num) {
-        int temp1 = num;
-        int digits = 0;
-
-        // Count digits
-        if (num == 0) {
-            digits = 1;
-        } else {
-            while (temp1 > 0) {
-                digits++;
-                temp1 /= 10;
-            }
-        }
-
-        int temp2 = num;
-        int sum = 0;
-
-        // Armstrong calculation (using Math.pow, close to your logic)
-        while (temp2 > 0) {
-            int digit = temp2 % 10;
-            sum += (int) Math.pow(digit, digits);
-            temp2 /= 10;
-        }
+        int digits = countDigits(num);
+        int sum = calculateArmstrongSum(num, digits);
 
         if (sum == num) {
             System.out.println("Armstrong Number");
