215d4964c08eb6f5eef538f52332981bd9aab1fe
Yash Izate <yashizate.softech@gmail.com>
2026-01-11 19:20:36 +0530

    added function

--- src/challenges/LcmOfTwo.java
+++ src/challenges/LcmOfTwo.java
@@ -3,6 +3,27 @@ package challenges;
 import java.util.Scanner;
 
 public class LcmOfTwo {
+
+    public static int userInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        return sc.nextInt();
+    }
+
+    public static void LCM(int a, int b) {
+        if (a == 0 || b == 0) {
+            System.out.println("LCM for zero ot defined");
+        } else {
+            for (int i = Math.max(a, b); i <= (a * b); i += Math.max(a, b)) {
+                if ((i % a == 0) && (i % b == 0)) {
+                    System.out.println("LCM = " + i);
+                    break;
+                }
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
