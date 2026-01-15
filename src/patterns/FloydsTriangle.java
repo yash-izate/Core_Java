af5bc8872aaae557439f19ec28e7895d0f47aa48
Yash Izate <yashizate.softech@gmail.com>
2026-01-15 12:20:35 +0530

    patters

---
+++ src/patterns/FloydsTriangle.java
@@ -0,0 +1,15 @@
+package patterns;
+
+public class FloydsTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        int count = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(count++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/patterns/FullPyramid.java
@@ -0,0 +1,23 @@
+package patterns;
+
+//____*
+//___* *
+//__* * *
+//_* * * *
+//* * * * *
+
+public class FullPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
