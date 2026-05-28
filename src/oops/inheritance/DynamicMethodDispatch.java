74905e8287ebd565202112dfc24d8bb9b4a3f30a
Yash Izate <yashizate.softech@gmail.com>
2026-05-28 18:47:51 +0530

    crt practice

--- src/oops/inheritance/DynamicMethodDispatch.java
+++ src/oops/inheritance/DynamicMethodDispatch.java
@@ -1,4 +1,35 @@
 package oops.inheritance;
 
-public class DynamaicMethodDispatch {
+class Phone {
+
+    public void showTime() {
+        System.out.println("It is 12:30 pm");
+    }
+
+    public void on() {
+        System.out.println("Turning on phone.");
+    }
+}
+
+class Smartphone extends Phone {
+    public void music() {
+        System.out.println("Tera Dhyan Kidhar...");
+    }
+
+    public void on() {
+        System.out.println("Turning on smartphone");
+    }
+}
+
+public class DynamicMethodDispatch {
+    public static void main(String[] args) {
+//        Phone ob1 = new Phone();                   //Allowed
+//        Smartphone ob2 = new Smartphone();         //Allowed
+//        Smartphone obj = new Phone();              //Not Allowed
+
+        Phone ob3 = new Smartphone();
+        ob3.on();
+        ob3.showTime();
+
+    }
 }
--- src/oops/inheritance/MethodOverriding.java
+++ src/oops/inheritance/MethodOverriding.java
@@ -1,4 +1,27 @@
 package oops.inheritance;
 
+class Base2 {
+    int a;
+
+    public void name() {
+        System.out.println("Yash");
+    }
+
+}
+
+class Derived2 extends Base2 {
+    int b;
+
+    @Override
+    public void name() {
+        System.out.println("Izate");
+    }
+}
+
 public class MethodOverriding {
+    public static void main(String[] args) {
+        Derived2 d1 = new Derived2();
+        d1.name();
+
+    }
 }
