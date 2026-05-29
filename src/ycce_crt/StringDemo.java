4bca358cae52b952346d90d1efe9f395380638de
Yash Izate <yashizate.softech@gmail.com>
2026-05-29 10:59:24 +0530

    crt practice

--- src/ycce_crt/StringDemo.java
+++ src/ycce_crt/StringDemo.java
@@ -1,12 +1,22 @@
 package ycce_crt;
 
 public class StringDemo {
+
     public static void main(String[] args) {
 
-        String str = "Yeshwantrao";
+        String str = "level";
+        String temp = "";
 
         for (int i = str.length() - 1; i >= 0; i--) {
-            System.out.println(str.charAt(i));
+            temp += str.charAt(i);
+        }
+
+        System.out.println(temp);
+
+        if (str.equals(temp)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
         }
     }
 }
