2861b4b11a430df4c07a5e2bbbf5686a45be31b8
Yash Izate <yashizate.softech@gmail.com>
2026-05-30 11:30:55 +0530

    crt practice

--- src/ycce_crt/ArrayDemo.java
+++ src/ycce_crt/ArrayDemo.java
@@ -23,9 +23,35 @@ public class ArrayDemo {
         }
     }
 
+    public static void dsa2(int[] arr) {
+
+        int id = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            boolean found = false;
+            
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] < arr[j]) {
+                    System.out.println(arr[i] + " -> " + arr[j]);
+                    found = true;
+                    break;
+                }
+
+            }
+
+            if (!found) {
+                System.out.println(arr[i] + " -> " + "-1");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = {3, 0, 1, 0, 0, 8, 4};
-        dsa1(arr);
+        int[] arr1 = {3, 0, 1, 0, 0, 8, 4};
+//        dsa1(arr1);
+
+        int[] arr2 = {2, 1, 6, 5};
+        dsa2(arr2);
 
     }
 }
