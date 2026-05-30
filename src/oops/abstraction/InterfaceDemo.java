e10b3a8bb0f4d38cbf646e3a809e8f7262f3bc15
Yash Izate <yashizate.softech@gmail.com>
2026-05-30 10:18:40 +0530

    crt practice

---
+++ src/oops/abstraction/InterfaceDemo.java
@@ -0,0 +1,59 @@
+package oops.abstraction;
+
+interface Bicycle {
+    int a = 45;
+
+    void applyBrakes(int decrement);
+
+    void speedUp(int increment);
+}
+
+interface HornBicycle {
+
+    void blowHorn3kg();
+
+    void blowHornmhn();
+
+}
+
+class AvonCycle implements Bicycle, HornBicycle {
+    void blowHorn() {
+        System.out.println("Horn Pee Poo");
+    }
+
+    @Override
+    public void blowHorn3kg() {
+        System.out.println("Pik Pik");
+    }
+
+    @Override
+    public void blowHornmhn() {
+        System.out.println("Bhooo Bhooo");
+    }
+
+    @Override
+    public void applyBrakes(int decrement) {
+        System.out.println("Applying Brakes");
+    }
+
+    @Override
+    public void speedUp(int increment) {
+        System.out.println("Applying Speed");
+
+    }
+}
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+
+        AvonCycle cycleHarry = new AvonCycle();
+        cycleHarry.applyBrakes(10);
+        cycleHarry.speedUp(20);
+
+//     you can create properties in Interfaces
+        System.out.println(cycleHarry.a);
+
+//     you cannot modify the properties in Interfaces as they are final
+
+    }
+}
