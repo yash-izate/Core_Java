c5a6ff7d4935000c6ce40d8d7072c9fa7548c892
Yash Izate <yashizate.softech@gmail.com>
2026-05-31 20:50:29 +0530

    crt practice

---
+++ src/multithreading/RunnableDemo.java
@@ -0,0 +1,32 @@
+package multithreading;
+
+import java.util.*;
+
+class MyThreadRunnable1 implements Runnable {
+    public void run() {
+        while (true) {
+            System.out.println("I am a thread 1 not a threat 1");
+        }
+    }
+}
+
+class MyThreadRunnable2 implements Runnable {
+    public void run() {
+        while (true) {
+            System.out.println("I am a thread 2 not a threat 2");
+        }
+    }
+}
+
+public class Runnable {
+    public static void main(String[] args) {
+        MyThreadRunnable1 bullet1 = new MyThreadRunnable1();
+        Thread gun1 = new Thread(bullet1);
+
+        MyThreadRunnable2 bullet2 = new MyThreadRunnable2();
+        Thread gun2 = new Thread(bullet2);
+
+        gun1.start();
+        gun2.start();
+    }
+}
