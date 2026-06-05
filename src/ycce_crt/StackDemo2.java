29039c5bef461d5584d14f4d16067067f76e2082
Yash Izate <yashizate.softech@gmail.com>
2026-06-05 11:11:31 +0530

    crt practice

---
+++ src/ycce_crt/StackDemo2.java
@@ -0,0 +1,27 @@
+package ycce_crt;
+
+import java.util.Stack;
+
+public class StackDemo2 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3, 1, 5, 4};
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            while (!st.isEmpty() && st.peek() < arr[i]) {
+                System.out.println(st.peek() + " -> " + arr[i]);
+                st.pop();
+            }
+            st.push(arr[i]);
+        }
+
+        while (!st.isEmpty()) {
+            System.out.println(st.peek() + " -> -1");
+            st.pop();
+        }
+        
+    }
+}
