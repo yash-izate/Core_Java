4b7b334d474396df5def76cfaba60404aca7e750
Yash Izate <yashizate.softech@gmail.com>
2026-06-06 10:48:59 +0530

    crt practice

---
+++ src/ycce_crt/QueueDemo3.java
@@ -0,0 +1,30 @@
+package ycce_crt;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo3 {
+    public static void main(String[] args) {
+        Queue<Character> que = new LinkedList<>();
+
+        int[] freq = new int[256];
+        String str = "aabbbcdmdcn";
+
+        for (int i = 0; i < str.length(); i++) {
+            que.offer(str.charAt(i));
+            freq[str.charAt(i)]++;
+
+        }
+
+        System.out.println(que);
+
+        while (!que.isEmpty()) {
+            if (freq[que.peek()] == 1) {
+                System.out.println("First non-repeating element: " + que.peek());
+                return;
+            }
+            que.poll();
+        }
+
+    }
+}
