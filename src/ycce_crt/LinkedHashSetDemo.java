5986850541f667e2ba0ea052e94d79065833fa08
Yash Izate <yashizate.softech@gmail.com>
2026-06-06 11:49:17 +0530

    crt practice

---
+++ src/ycce_crt/LinkedHashSetDemo.java
@@ -0,0 +1,22 @@
+package ycce_crt;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> st = new LinkedHashSet<>();
+
+        st.add(10);
+        st.add(20);
+        st.add(10);
+        st.add(40);
+        st.add(50);
+        st.add(40);
+        st.add(20);
+        st.add(30);
+        st.add(50);
+
+        System.out.println(st);
+        System.out.println("Unique Element: " + st.size());
+    }
+}
